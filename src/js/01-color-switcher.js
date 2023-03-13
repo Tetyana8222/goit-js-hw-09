96882c653892f3133c99a464225611ec33791e94
Tetyana Kolisnyk <kolesnik.tm@gmail.com>
2023-03-13 19:20:34 +0300

    task- 01 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
 
+const PROMPT_INTERVAL = 1000;
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const colorSwitcher = () => {
+  body.style.backgroundColor = getRandomHexColor();
+};
+startBtn.addEventListener('click', () => {
+  timerId = setInterval(colorSwitcher, PROMPT_INTERVAL);
+  startBtn.disabled = true;
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  startBtn.disabled = false;
+});
