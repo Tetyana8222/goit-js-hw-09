402f4316ab263f8a603e413de4e66f07942d44e6
Tetyana Kolisnyk <kolesnik.tm@gmail.com>
2023-03-17 11:55:24 +0300

    task 03 in progress

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,17 +1,43 @@
 import Notiflix from 'notiflix';
 
 const form = document.querySelector('.form');
-const firstDelayMs = document.querySelector('[name="delay"]');
-const delayStepMs = document.querySelector('[name="step"]');
-const amount = document.querySelector('[name="amount"]');
-
-form.addEventListener('submit', createPromise);
+let delayInput = null;
+let stepInput = null;
+let amountInput = null;
+// const firstDelayMs = document.querySelector('[name="delay"]');
+// const delayStepMs = document.querySelector('[name="step"]');
+// const amount = document.querySelector('[name="amount"]');
 
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+form.addEventListener('submit', handleSubmit);
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { delay, step, amount },
+  } = event.currentTarget;
+  let delayInput = Number(delay.value);
+  let stepInput = Number(step.value);
+  let amountInput = Number(amount.value);
+
+  for (let i = 1; i <= amountInput; i += 1) {
+    createPromise(i, delayInput)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
+  event.currentTarget.reset();
 }
